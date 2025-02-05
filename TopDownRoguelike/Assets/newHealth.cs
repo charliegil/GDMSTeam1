6b238d79f36d576a9eb44172938ea2eae5065b89
ol1oly <olivier.girardville@outlook.com>
2025-02-04 20:01:41 -0500

    made health ui

---
+++ TopDownRoguelike/Assets/newHealth.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class newHealth : MonoBehaviour
+{
+    public float regenerationPerSecond= 0;
+    public float totalHP =100;
+
+    public float currentHP = 100;
+
+    public bool regenerate = false;
+
+    public Slider sliderChange;
+
+    public Slider HealthBar;
+
+    public float animationDuration = 0.1f;
+
+    private float time= 0;
+    public TMP_Text Val;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        setHP(sliderChange.value*totalHP);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - time >=1f && regenerate){
+            time = Time.time;
+            setHP(currentHP+regenerationPerSecond);
+            //sliderChange.value = currentHP;
+        }
+    }
+
+    void setHP(float hp){
+        if(hp> totalHP) hp = totalHP;
+        currentHP = hp;
+        StartCoroutine(LerpHealthBar(HealthBar.value, (float)hp/totalHP));
+        Val.text = (int)hp+"/"+totalHP;
+        
+    }
+    public void onValueChange(){
+        float value = sliderChange.value;
+        setHP(value*totalHP);
+        
+    }
+   
+    IEnumerator LerpHealthBar(float start, float end)
+    {
+        float timeElapsed = 0f;
+
+        while (timeElapsed < animationDuration)
+        {
+            HealthBar.value = Mathf.Lerp(start, end, timeElapsed / animationDuration);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        HealthBar.value = end; 
+    }
+    void addHP(float add){
+        setHP(currentHP+add);
+    }
+}
