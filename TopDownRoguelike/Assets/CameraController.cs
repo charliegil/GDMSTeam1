4387b71e62f3d70a1a9852d3d9b3c65b988d6ba7
AcacieS <acaciesong@gmail.com>
2025-02-06 17:38:28 -0500

    fix some size

--- TopDownRoguelike/Assets/CameraController.cs
+++ TopDownRoguelike/Assets/CameraController.cs
@@ -7,17 +7,18 @@ public class CameraController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player");
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        player = Player.ActivePlayer.gameObject;
-        if (player != null){
-         transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
-        }
+        // player = Player.ActivePlayer.gameObject;
+        // if (player != null){
+        //  transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        // }
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
     }
     
 }
