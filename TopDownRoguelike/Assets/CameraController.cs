baa602cedc459f3332441de879cce17853cca311
Charlie Gil <charliegil0691@gmail.com>
2025-02-06 12:13:24 -0500

    Camera follows player

--- TopDownRoguelike/Assets/CameraController.cs
+++ TopDownRoguelike/Assets/CameraController.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class CamerController : MonoBehaviour
 {
+    [SerializeField] GameObject player;
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        transform.position = new Vector3(playerPos.x, playerPos.y, transform.position.z);
     }
 }
