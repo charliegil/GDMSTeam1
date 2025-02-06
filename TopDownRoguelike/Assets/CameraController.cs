37f0e9f54fe9d31c395b1bc5b41f2fe0ebce6405
Charlie Gil <charliegil0691@gmail.com>
2025-02-06 12:43:50 -0500

    Stable version (moving camera, no null enemy reference))

---
+++ TopDownRoguelike/Assets/CameraController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
