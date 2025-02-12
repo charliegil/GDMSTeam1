29af0e4c331adb9a9d44338db41170995d4700e0
AcacieS <acaciesong@gmail.com>
2025-02-12 14:43:08 -0500

    a basic patrol between 2 point

--- TopDownRoguelike/Assets/Script/player_transform/tongControl.cs
+++ TopDownRoguelike/Assets/Script/player_transform/tongControl.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class tongControl : Player, IControllable
 {
-
+    public GameObject pointA;
+    public GameObject pointB;
+    private Transform currentPoint;
     [Header("Movement Speed")]
     [SerializeField] private float walkSpeed = 3.0f;
 
@@ -11,6 +13,8 @@ public class tongControl : Player, IControllable
     
     private void Awake(){
         rb = GetComponent<Rigidbody2D>();
+        currentPoint = pointB.transform;
+
         inputHandler = GameObject.Find("PlayerInputHandler").GetComponent<PlayerInputHandler>();
     }
     private void Update(){
@@ -24,11 +28,29 @@ public class tongControl : Player, IControllable
         if(ActivePlayer==this){
             Vector2 inputDirection = new Vector2(inputHandler.MoveInput.x, inputHandler.MoveInput.y);
             currentMvt = inputDirection.normalized * walkSpeed;
+        }else{
+            Debug.Log("hey should move");
+            Vector2 point = currentPoint.position - transform.position;
+            if(currentPoint == pointB.transform){
+                rb.linearVelocity = new Vector2(walkSpeed, 0);
+            }else{
+                rb.linearVelocity = new Vector2(-walkSpeed, 0);
+            }
+            if(Vector2.Distance(transform.position, currentPoint.position)< 1f && currentPoint == pointB.transform){
+                currentPoint = pointA.transform;
+                Debug.Log("B");
+            }
+            if(Vector2.Distance(transform.position, currentPoint.position)< 1f && currentPoint == pointA.transform){
+                currentPoint = pointB.transform;
+                 Debug.Log("A");
+            }
         }
     }
 
    private void FixedUpdate(){
+    if(ActivePlayer==this){
         rb.linearVelocity = currentMvt;
+    }
    }
    
 }
