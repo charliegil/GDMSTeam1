3a03a69ce228ba678d5f36b5c16b8184f5fff05d
ol1oly <olivier.girardville@outlook.com>
2025-02-12 21:59:28 -0500

    updating playerController

--- TopDownRoguelike/Assets/PlayerController.cs
+++ TopDownRoguelike/Assets/PlayerController.cs
@@ -16,7 +16,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     private Vector2 movementDirection;
     private Vector2 currentMovement;
+    private Vector2 externalVelocity = new Vector2(0, 0);
     private float currentSpeed;
+
+    bool canMove = true;
+
 
     // ===================== PHASING =====================
     [SerializeField] private float phaseDuration = 1f;
@@ -95,7 +99,12 @@ public class PlayerController : MonoBehaviour
 
     private void Move() {
         currentMovement = movementDirection * currentSpeed;
-        rb.linearVelocity = currentMovement;
+        if(canMove){
+            rb.linearVelocity = currentMovement + externalVelocity;
+        }
+        else{
+            rb.linearVelocity = externalVelocity;
+        }
     }
 
     private void Attack() {
@@ -150,6 +159,16 @@ public class PlayerController : MonoBehaviour
 
         return closest;
     }
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Projectile")) {
+            Debug.Log("I'm hit!");
+            TakeDamage(1);
+        }
+    }
+    public void TakeDamage(float damage) {
+        //health -= damage;
+        //healthText.SetText(health.ToString());
+    }
 
     // ===================== COROUTINES =====================
     private IEnumerator Phase() {
@@ -191,7 +210,7 @@ public class PlayerController : MonoBehaviour
         while (true) {
 
             if (targetEnemy != null && targetEnemy.gameObject != null) {
-                targetEnemy.GetComponent<Health>().TakeDamage(10);
+                //targetEnemy.GetComponent<Health>().TakeDamage(10);
             } else {
                 yield break;
             }
@@ -199,7 +218,71 @@ public class PlayerController : MonoBehaviour
             yield return new WaitForSeconds(0.5f);
         }
     }
+public void setExternalVelocity(Vector2 vec){
+        externalVelocity = vec;
+    }
+    public void addExternalVelocity(Vector2 vec){
+        externalVelocity+=vec;
+    }
+    public Vector2 getExternalVelocity(){return externalVelocity;}
 
+    public void SetTimerVelocity(float time, Vector2 direction, bool canMoveDuring){ // this function applies a velocity for a fixed amount of time
+        StartCoroutine(SetTimeVelocityEnumerator(time, direction, canMoveDuring));
+    }
+    private IEnumerator SetTimeVelocityEnumerator(float time, Vector2 direction, bool canMoveDuring){
+            bool before = canMove;
+            canMove = canMoveDuring;;
+            addExternalVelocity(direction);
+            
+            yield return new WaitForSeconds(time);
+
+            addExternalVelocity(-direction);
+            canMove = before;
+    }
+    
+    
+    public void MovePlayerTowards(float time, Vector2 point , bool canMoveDuring){
+        StartCoroutine(MovePlayerTowardsEnumerator(time,point,canMoveDuring));
+    }
+    private IEnumerator MovePlayerTowardsEnumerator(float time, Vector3 point, bool canMoveDuring){
+        // this function will move the player towards a certain point. it will adjust every delta time to make sure it goes towards that point
+        // if time is 0 , this function will stop when it reach that point
+        // we cant just use Vector2.MoveTowards() because we are using velocity to move the player
+            bool before = canMove;
+            canMove = canMoveDuring;
+            float currentTime = 0;
+            Vector2 oldDirection  = new Vector2(0,0);
+            
+            while((time ==-1 && (transform.position-point).magnitude > 0.1) || (time != -1 && currentTime < time)){
+                Vector3 direction = (transform.position - point).normalized;
+                addExternalVelocity(-oldDirection);
+                
+                addExternalVelocity(direction * Time.deltaTime * 10); // need to make 10 into an actual variable
+                oldDirection = (direction * Time.deltaTime * 10);
+                currentTime += Time.deltaTime;
+                yield return null;
+
+            }
+            addExternalVelocity(-oldDirection);
+            canMove = before;
+    }
+    public void setCanMove(bool move){
+        canMove = move;
+    }
+    public void applyMultiplier(skillTreeUpgrade sku){
+        if(sku.getId() == 1){ // for example, apply multiplier on health
+
+        }
+        else if(sku.getId() == 1){ // for example, apply multiplier on the dash speed
+
+        }
+        else if(sku.getId() == 1){ // for example, apply multiplier on the dash cooldown
+
+        }
+        else if(sku.getId() == 1){ // for example, apply multiplier on overall speed
+
+        }
+    }
     // ===================== DEBUGGING =====================
     // void OnDrawGizmos() {
     //     Handles.DrawWireDisc(transform.position, Vector3.forward, attackRange);
