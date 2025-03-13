b9cdb182baac8fb24ddbe5b9e52dd3164ab376fb
AcacieS <acaciesong@gmail.com>
2025-03-13 12:38:23 -0400

    bullet detect CapsuleCollider2D

--- TopDownRoguelike/Assets/Bullet.cs
+++ TopDownRoguelike/Assets/Bullet.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision){
-        if(collision.gameObject.layer==3){
-            
-            Debug.Log("detect enemy");
+        
+        if(collision.gameObject.layer==3 && collision is CapsuleCollider2D){
+
+            Debug.Log("detect enemy ");
             Destroy(gameObject);
             collision.gameObject.GetComponent<Health>().TakeDamage(10);
             //Destroy(collision.gameObject);
