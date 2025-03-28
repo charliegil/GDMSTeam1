9e698de68445eca69510d379729ab628f7c65748
mondaydavid <mondaydevid@gmail.com>
2025-03-28 12:55:42 -0400

    put in audio settings

---
+++ TopDownRoguelike/Assets/SoundManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolune")) {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+
+        else {
+        Load();
+        }
+    }
+
+    public void ChangeVolume() {
+        AudioListener.volume = volumeSlider.value;
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save() {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+    }
+
+
+}
